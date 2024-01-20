519d05ceec58421224cee2fb5be91a67ca2e7022
Ashu Gupta <ashugpt1812@gmail.com>
2024-01-20 22:06:20 +0530

    removed separate register page

--- app/permit/page.tsx
+++ app/permit/page.tsx
@@ -6,15 +6,21 @@ import { useAccount } from 'wagmi';
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Approve from './approve';
-import Register from './register';
 import Navbar from '../Navbar';
+import { createBrowserClient } from '@supabase/ssr'
 
 const Permit = () => {
+
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
 
   const { address, isConnected, isConnecting, isDisconnected } = useAccount();
   const [email, setEmail] = useState('');
   const [amount, setAmount] = useState('');
-  const [walletAddress, setWalletAddress] = useState('');
+  const [connectedWalletAddress, setConnectedWalletAddress] = useState('');
+  const [registeredWalletAddress, setRegisteredWalletAddress] = useState('');
   const [sum, setSum] = useState();
   const [deadline, setDeadline] = useState('');
   const [v, setV] = useState('');
@@ -22,17 +28,70 @@ const Permit = () => {
   const [s, setS] = useState('');
 
   useEffect(() => {
-    address && setWalletAddress(address);
+    const fetchUserEmail = async () => {
+      try {
+        const response = await fetch('/api/', {method: 'POST', headers: {
+          'Content-Type': 'application/json'
+        },});
+        const data = await response.json();
+        setEmail(data.userEmail);
+        return data.userEmail;
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    const fetchWalletAddress = async (userEmail: string) => {
+      try {
+          let { data, error } = await supabase
+          .from('email_to_address')
+          .select()
+          .eq('email', userEmail);
+          if(data) {
+            setRegisteredWalletAddress(data[0].address);
+          } else {
+            console.log(data);
+          }
+      } catch (error) {
+          console.error('Error:', error);
+      }
+    };
+
+    fetchUserEmail()
+    .then((userEmail) => fetchWalletAddress(userEmail));
+
+  }, []);
+
+  useEffect(() => {
+    address && setConnectedWalletAddress(address);
   }, [address])
+
+  const handleRegister = async () => {
+    try {
+        const { data, error } = await supabase
+        .from('email_to_address')
+        .upsert({ 'email': email, 'address': connectedWalletAddress }, { onConflict: 'email' })
+        .select();
+        
+        if(data) console.log('data', data);
+        if(error) console.log('supabase error', error);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   return (
     <>
       <Navbar/>
       <div>
-        <Register walletAddress={walletAddress} />
+        <p>Register your wallet address</p>
+        <Input placeholder="Email" value={email} disabled />
+        <Input placeholder="Connect to Wallet to Register Wallet Address" value={connectedWalletAddress} readOnly />
+        <Button onClick={handleRegister}>Register</Button>
         <p>Permit</p>
+        <Input placeholder="Registered Wallet Address" value={registeredWalletAddress} disabled />
         <Input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
-        <Approve props={ sum, amount, deadline, v, r, s } />
+        {/* <Approve props={ sum, amount, deadline, v, r, s } /> */}
       </div>
     </>
   );
